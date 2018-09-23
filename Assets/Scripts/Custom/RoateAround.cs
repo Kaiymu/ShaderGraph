49d5f5483273b788f0342157a17b125ee1ac7459
Tfontaine <tom_1993@hotmail.fr>
2018-09-23 23:15:53 +0200

    [CODE / MATERIALS / SHADERS] - Initial commit containings shaders made from Brackeys tutorial / myself teaching ShaderGraph

---
+++ Assets/Scripts/Custom/RoateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoateAround : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _objectToRotate;
+
+    [SerializeField]
+    private float _speed = 1f;
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+        transform.LookAt(_objectToRotate);
+        transform.Translate(Vector3.right * Time.deltaTime * _speed);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time), transform.position.z);
+	}
+}
